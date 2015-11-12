45271ed3748d4ca8519eaccfbc2c9c623b48ef00
Josh <jkim5674@gmail.com>
2015-11-11 22:21:47 -0800

    updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :category
-	has_many :comments
+	has_many :comments, :dependent => :destroy
+	
 	validates :name, :pricing, :description, presence: true
 end
